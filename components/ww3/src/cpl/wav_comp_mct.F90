ab2aed96498275b285230bf325a08156e3d0aeef
Erin Thomas <ac.ethomas@chrlogin1.lcrc.anl.gov>
2023-11-02 16:23:03 -0500

    modification to WW3 infrastructure for ice coupling. Default output now includes full spectra.

--- components/ww3/src/cpl/wav_comp_mct.F90
+++ components/ww3/src/cpl/wav_comp_mct.F90
@@ -133,11 +133,11 @@
       use w3gdatmd, only: dtmax, dtcfl, dtcfli, dtmin, &
                           nx, ny, nsea, nseal, mapsf, mapfs, mapsta, mapst2, x0, y0, sx, sy, xgrd, ygrd, &
                           w3nmod, w3setg, AnglD, &
-                          sig, nk, zb, dmin, &
+                          sig, nk, zb, dmin, xfr, fr1, &
                           usspf
       use w3wdatmd, only: time, w3ndat, w3setw, wlv, va, ust, ice 
       use w3adatmd, only: ussp, w3naux, w3seta, sxx, sxy, syy, fliwnd, flcold, dw, cg, wn, hs, fp0, thp0, &
-                          charn, z0,ustar2, tauwix, tauwiy, tauox, tauoy, tauocx, tauocy
+                          charn, z0,ustar2, tauwix, tauwiy, tauox, tauoy, tauocx, tauocy, ef
       use w3idatmd, only: inflags1, inflags2,w3seti, w3ninp
       USE W3IDATMD, ONLY: TC0, CX0, CY0, TCN, CXN, CYN, ICEP1, ICEP5, TI1, TI5
       USE W3IDATMD, ONLY: TW0, WX0, WY0, DT0, TWN, WXN, WYN, DTN
@@ -167,7 +167,8 @@
       use seq_flds_mod
 
       use ww3_cpl_indices  , only : ww3_cpl_indices_set
-      use ww3_cpl_indices  , only : index_x2w_Sa_u, index_x2w_Sa_v, index_x2w_Sa_tbot, index_x2w_Si_ifrac, index_x2w_si_ithick
+      use ww3_cpl_indices  , only : index_x2w_Sa_u, index_x2w_Sa_v, index_x2w_Sa_tbot
+      use ww3_cpl_indices  , only : index_x2w_Si_ifrac, index_x2w_Si_ithick, index_x2w_Si_ifloe
       use ww3_cpl_indices  , only : index_x2w_So_t, index_x2w_So_u, index_x2w_So_v, index_x2w_So_bldepth, index_x2w_So_ssh
       use ww3_cpl_indices  , only : index_w2x_Sw_ustokes_wavenumber_1, index_w2x_Sw_vstokes_wavenumber_1, &
                                     index_w2x_Sw_ustokes_wavenumber_2, index_w2x_Sw_vstokes_wavenumber_2, &
@@ -179,7 +180,7 @@
                                     index_w2x_Sw_Charn, index_w2x_Sw_Ustar, index_w2x_Sw_Z0,              &
                                     index_w2x_Faww_Tawx, index_w2x_Faww_Tawy, index_w2x_Fwow_Twox,        &
                                     index_w2x_Fwow_Twoy, index_w2x_Faow_Tocx, index_w2x_Faow_Tocy
-
+      use ww3_cpl_indices  , only : index_w2x_Sw_wavespec                      
 
       use shr_sys_mod      , only : shr_sys_flush, shr_sys_abort
       use shr_kind_mod     , only : in=>shr_kind_in, r8=>shr_kind_r8, &
@@ -976,6 +977,7 @@ CONTAINS
       integer :: tod              ! current time of day (sec)
       integer :: hh,mm,ss
       integer :: n,jsea,isea
+      integer :: ifreq
       integer :: mpi_comm
       integer :: gindex
       integer :: nu
@@ -1152,9 +1154,9 @@ CONTAINS
          endif
 
          if (inflags1(4)) then
-            ICEI(IX,IY) = x2w0%rattr(index_x2w_si_ifrac,gindex)
-            ICEP1(IX,IY) = x2w0%rattr(index_x2w_si_ithick,gindex)
-            !ICEP5(IX,IY) = x2w0%rattr(index_x2w_si_ifloe,gindex)
+            ICEI(IX,IY) = x2w0%rattr(index_x2w_Si_ifrac,gindex)
+            ICEP1(IX,IY) = x2w0%rattr(index_x2w_Si_ithick,gindex)
+            ICEP5(IX,IY) = x2w0%rattr(index_x2w_Si_ifloe,gindex)
 
          endif
 
@@ -1217,6 +1219,10 @@ CONTAINS
 
                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = USSP(jsea,6)
                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = USSP(jsea,nk+6)
+               
+               do ifreq=1,nk 
+                  w2x_w%rattr(index_w2x_Sw_wavespec(ifreq),jsea) = EF(jsea,ifreq)
+               enddo
             endif
          else
             if (wav_ocn_coup .eq. 'twoway' .or. wav_atm_coup .eq. 'twoway') then
@@ -1253,6 +1259,10 @@ CONTAINS
 
                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = 0.0
                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = 0.0
+               
+               do ifreq=1,nk 
+                  w2x_w%rattr(index_w2x_Sw_wavespec(ifreq),jsea) = 0.0
+               enddo
             endif
          endif
       enddo
