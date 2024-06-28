01bd1ec8f9057a87dbee977f9b16fbb6a91401fa
Erin Thomas <ac.ethomas@chrlogin2.lcrc.anl.gov>
2024-06-28 10:59:10 -0500

    more fixes to wav_ocn_coup and wav_ice_coup flags

--- components/ww3/src/cpl/wav_comp_mct.F90
+++ components/ww3/src/cpl/wav_comp_mct.F90
@@ -1219,7 +1219,8 @@ CONTAINS
 
                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = USSP(jsea,6)
                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = USSP(jsea,nk+6)
-               
+            endif
+            if (wav_ice_coup .eq. 'twoway') then 
                do ifreq=1,nk 
                   w2x_w%rattr(index_w2x_Sw_wavespec(ifreq),jsea) = EF(jsea,ifreq)
                enddo
@@ -1259,7 +1260,8 @@ CONTAINS
 
                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = 0.0
                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = 0.0
-               
+             endif
+             if (wav_ice_coup .eq. 'twoway') then
                do ifreq=1,nk 
                   w2x_w%rattr(index_w2x_Sw_wavespec(ifreq),jsea) = 0.0
                enddo
